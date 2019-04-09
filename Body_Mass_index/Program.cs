d8f7a24dd88c96056c5c1e7c56a1be07d4b5450d
Konstantin Oleksyuk <cp.shotover@gmail.com>
2019-04-09 18:39:06 +0300

    Add project files.

---
+++ Body_Mass_index/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Body_Mass_index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
